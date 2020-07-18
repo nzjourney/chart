73c111d7f83826971d2eea77de62b5a2c884b8df
Nizar Lazuardy Firmansyah <nizar.firmansyah@metraplasa.co.id>
2020-07-18 19:18:10 +0700

    fix dot

--- index.sh
+++ index.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-helm repo index --url https://nzjourney.github.io/chart/ #--merge index.yaml .
+#helm repo index --url https://nzjourney.github.io/chart/ --merge index.yaml .
+helm repo index --url https://nzjourney.github.io/chart/ .
